56af018d2321ac048bebba6d355af471470c9c31
Maryna Voitenko <maryna.voitenko@gmail.com>
2018-08-02 21:42:59 -0400

    create create controller action for messages

--- message-board-api/app/controllers/messages_controller.rb
+++ message-board-api/app/controllers/messages_controller.rb
@@ -4,6 +4,12 @@ class MessagesController < ApplicationController
     end
 
     def create
+        message = Message.new(message_params)
+        if message.save
+            render json: message
+        else
+            render json: { message: 'Message was not created' }, status: 422
+        end
     end
 
     def show
