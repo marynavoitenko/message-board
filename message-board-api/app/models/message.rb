609be829153b31e6537f61b47f3182609b184da1
Maryna Voitenko <maryna.voitenko@gmail.com>
2018-08-04 14:06:03 -0400

    make sender and recipient not required on message create

--- message-board-api/app/models/message.rb
+++ message-board-api/app/models/message.rb
@@ -1,6 +1,6 @@
 class Message < ApplicationRecord
-    belongs_to :sender, :class_name => 'User', :foreign_key => 'sender_id'
-    belongs_to :recipient, :class_name => 'User', :foreign_key => 'recipient_id'
+    belongs_to :sender, :class_name => 'User', :foreign_key => 'sender_id', required: false
+    belongs_to :recipient, :class_name => 'User', :foreign_key => 'recipient_id', required: false
 
     validates :content, presence: true
 end
