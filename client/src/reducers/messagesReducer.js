eafaaae22b527ac0618fb945e34cd0f7935cf47f
Maryna Voitenko <maryna.voitenko@gmail.com>
2018-08-05 16:42:20 -0400

    add loading_messages to messages reducer

--- client/src/reducers/messagesReducer.js
+++ client/src/reducers/messagesReducer.js
@@ -1,11 +1,23 @@
-export default (state = [], action) => {
+export default (state = {
+  isFetching: false,
+  messages: []
+}, action) => {
     switch (action.type) {
   
       case 'ADD_MESSAGE':
         return [ ...state, action.message ];
-  
+      
+      case 'LOADING_MESSAGES':
+        return {
+          ...state,
+          isFetching: true
+        }
+        
       case 'FETCH_MESSAGES':
-        return action.messages;
+        return {
+          messages: action.payload,
+          isFetching: false
+        }
   
       default:
         return state;
