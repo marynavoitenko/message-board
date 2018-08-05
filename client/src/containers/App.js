eac7be95a444d7c7a37b87f2b9607713dc6ea2c1
Maryna Voitenko <maryna.voitenko@gmail.com>
2018-08-05 14:51:45 -0400

    delete unused app.css and app.logo

--- client/src/containers/App.js
+++ client/src/containers/App.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
-import './App.css';
 import MessageBoardPage from './containers/MessageBoardPage';
 
 class App extends Component {
--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import App from './containers/App';
 import registerServiceWorker from './registerServiceWorker';
+import rootReducer from './reducers/index';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = createStore(rootReducer, applyMiddleware(thunk));
+
+ReactDOM.render(
+  <Provider store={store} >
+    <App />
+  </Provider>, document.getElementById('root')
+);
+
 registerServiceWorker();
