08f158cd7b103098d2ceaccad7873b7e3c1897a7
Maryna Voitenko <maryna.voitenko@gmail.com>
2018-08-05 13:30:42 -0400

    add message board page container

---
+++ client/src/containers/MessageBoardPage.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class MessageBoardPage extends Component {
+    render() {
+        return (
+            <div>Message Board Page</div>
+        );
+    }
+}
+
+export default MessageBoardPage;
