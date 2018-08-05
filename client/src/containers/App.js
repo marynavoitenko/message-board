dc32ee57a7c0d92c4400c5ab73bb11c1255aa609
Maryna Voitenko <maryna.voitenko@gmail.com>
2018-08-05 14:56:04 -0400

    import messages list into messageboardpage

--- client/src/containers/App.js
+++ client/src/containers/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import MessageBoardPage from './containers/MessageBoardPage';
 
 class App extends Component {
--- client/src/containers/MessageBoardPage.js
+++ client/src/containers/MessageBoardPage.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import MessagesList from '../components/MessagesList';
 
 class MessageBoardPage extends Component {
     render() {
