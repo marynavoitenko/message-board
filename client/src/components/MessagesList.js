5950111e86e126678c143f5b5645c51fa70b5b07
Maryna Voitenko <maryna.voitenko@gmail.com>
2018-08-05 14:02:23 -0400

    create MessagesList component

---
+++ client/src/components/MessagesList.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+
+const MessagesList = ({ messages }) => {
+  const renderMessages = messages.map(message => 
+    <li key={message.id}>{message.content}</li>
+  );
+  
+  return (
+    <ul>
+      {renderMessages}
+    </ul>
+  );
+};
+
+export default MessagesList;
