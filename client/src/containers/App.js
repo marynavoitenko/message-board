a13faac87a1016c4a381a2a6859f8eff70d1d564
Maryna Voitenko <maryna.voitenko@gmail.com>
2018-08-08 13:41:29 -0400

    set up router on app level

--- client/src/containers/App.js
+++ client/src/containers/App.js
@@ -1,10 +1,19 @@
 import React, { Component } from 'react';
 import MessageBoardPage from './MessageBoardPage';
+import { 
+  BrowserRouter as Router, 
+  Route 
+} from 'react-router-dom';
 
 class App extends Component {
   render() {
     return (
-      <MessageBoardPage />
+      <Router>
+        <div>
+          <Route exact path="/" render={() => <div>Home</div>} />
+          <Route path='/messages' component={MessageBoardPage}/>
+        </div>
+      </Router>
     );
   }
 }
