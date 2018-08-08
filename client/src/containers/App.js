50bb7d157347402338ec49f4c5e2f66b9fb5abd1
Maryna Voitenko <maryna.voitenko@gmail.com>
2018-08-08 13:49:24 -0400

    add navbar to app

--- client/src/containers/App.js
+++ client/src/containers/App.js
@@ -4,12 +4,14 @@ import {
   BrowserRouter as Router, 
   Route 
 } from 'react-router-dom';
+import NavBar from '../components/NavBar'
 
 class App extends Component {
   render() {
     return (
       <Router>
         <div>
+          <NavBar />
           <Route exact path="/" render={() => <div>Home</div>} />
           <Route path='/messages' component={MessageBoardPage}/>
         </div>
