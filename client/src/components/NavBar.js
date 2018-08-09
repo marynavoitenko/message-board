7e1fdd035698f39f776882823d040d8566cea73e
Maryna Voitenko <maryna.voitenko@gmail.com>
2018-08-08 22:02:22 -0400

    add navbar styles

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -3,7 +3,7 @@ import { NavLink } from 'react-router-dom';
 
 const NavBar = props => {
   return (
-    <div className="NavBar">
+    <div className="NavBar container">
       <NavLink 
         className="NavLink"
         to="/"
