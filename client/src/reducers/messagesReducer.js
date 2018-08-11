f25c4b5b215b425c433e5fc90eeb191ce4fa84ad
Maryna Voitenko <maryna.voitenko@gmail.com>
2018-08-11 16:51:18 -0400

    update reducer with delete actions

--- client/src/reducers/messagesReducer.js
+++ client/src/reducers/messagesReducer.js
@@ -1,6 +1,7 @@
 export default (state = {
   isFetching: false,
   isPosting: false,
+  isDeleting: false,
   messages: [],
   message: {}
 }, action) => {
@@ -29,6 +30,17 @@ export default (state = {
           ...state,
           message: state.messages.concat(action.payload)
         }
+
+      case 'DELETING_MESSAGE':
+        console.log(state.isDeleting);
+        return {
+          ...state,
+          isDeleting: true
+        } 
+      
+      case 'DELETE_MESSAGE_SUCCES':
+        const messages = state.messages.filter(message => message.id !== parseInt(action.deletedMessageId, 10));
+        return { messages };
 
       default:
         return state;
