5b49b57063c4dd1f47b9100daabe388c0662149f
Maryna Voitenko <maryna.voitenko@gmail.com>
2018-08-11 08:29:16 -0400

    remove unused container div

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -3,30 +3,28 @@ import { NavLink } from 'react-router-dom';
 
 const NavBar = props => {
   return (
-    <div className="container">
-      <div className="NavBar">
-        <NavLink 
-          className="NavLink"
-          to="/"
-          exact
-        >
-          Home
-        </NavLink>
-        <NavLink 
-          className="NavLink" 
-          to="/messages"
-          exact
-        >
-          Messages
-        </NavLink>
-        <NavLink 
-          className="NavLink"
-          to="/messages/new"
-          exact
-        >
-          Add Message
-        </NavLink>
-      </div>
+    <div className="NavBar">
+      <NavLink 
+        className="NavLink"
+        to="/"
+        exact
+      >
+        Home
+      </NavLink>
+      <NavLink 
+        className="NavLink" 
+        to="/messages"
+        exact
+      >
+        Messages
+      </NavLink>
+      <NavLink 
+        className="NavLink"
+        to="/messages/new"
+        exact
+      >
+        Add Message
+      </NavLink>
     </div>
   );
 }
