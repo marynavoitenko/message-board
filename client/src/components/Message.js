598101b50b6a76c97ce41bdd10c6f28ce286611b
Maryna Voitenko <maryna.voitenko@gmail.com>
2018-08-11 15:52:01 -0400

    pass message id from message to delete handler in parent container

--- client/src/components/Message.js
+++ client/src/components/Message.js
@@ -1,25 +1,25 @@
 import React from 'react';
 
-const Message = ({ message }) => {
-  const sender = message.sender;
-  const recipient = message.recipient;
+const Message = (props) => {
+  const { sender, recipient, content } = props.message;
 
   return (
     <div className="single-message-page">
         <div className="message-card message-card-fullpage">
             { (sender || recipient) ?
                 <div className="message-card-header">
-                {sender ? <span className="sender">From: {message.sender.name}</span> : null }
+                {sender ? <span className="sender">From: {sender.name}</span> : null }
 
-                {recipient ? <span className="recipient">To: {message.recipient.name}</span> : null }
+                {recipient ? <span className="recipient">To: {recipient.name}</span> : null }
                 </div>
                 : null
             }
 
             <div className="message-card-main">
-                {message.content}
+                {content}
             </div>
-            <button  className="delete-message-button">
+
+            <button onClick={props.clicked.bind(this, props.message.id)} className="delete-message-button">
                 Delete
             </button>
         </div>
