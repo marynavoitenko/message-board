cec5543ff7dec0af59b54fd8f6fc523df8028ec2
Maryna Voitenko <maryna.voitenko@gmail.com>
2018-08-16 22:24:15 -0400

    update store to use redux devtools

--- client/src/index.js
+++ client/src/index.js
@@ -8,7 +8,11 @@ import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 
-const store = createStore(rootReducer, applyMiddleware(thunk));
+const store = createStore(
+  rootReducer, 
+  applyMiddleware(thunk), 
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
 
 ReactDOM.render(
   <Provider store={store} >
