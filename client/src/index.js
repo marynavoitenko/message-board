a71bb9a0b05eb716b699c2397f820ec2bc4cebd8
Maryna Voitenko <maryna.voitenko@gmail.com>
2018-08-16 22:29:51 -0400

    fix redux devtools setup

--- client/src/index.js
+++ client/src/index.js
@@ -5,14 +5,15 @@ import App from './containers/App';
 import registerServiceWorker from './registerServiceWorker';
 import rootReducer from './reducers/index';
 import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
 const store = createStore(
-  rootReducer, 
-  applyMiddleware(thunk), 
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-);
+  rootReducer, composeEnhancers(
+    applyMiddleware(thunk)
+));
 
 ReactDOM.render(
   <Provider store={store} >
