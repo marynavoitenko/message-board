777c517c257b3936105de8b53ac97cae0cfac884
Maryna Voitenko <maryna.voitenko@gmail.com>
2018-08-19 22:19:56 -0400

    clean up message card

--- client/src/components/MessageCard.js
+++ client/src/components/MessageCard.js
@@ -1,8 +1,8 @@
 import React from 'react';
 
 const MessageCard = ({ message }) => {
-  const sender = message.sender;
-  const recipient = message.recipient;
+  const { sender, recipient } = message;
+  // const recipient = message.recipient;
 
   const longMessage = message.content.length > 140;
 
