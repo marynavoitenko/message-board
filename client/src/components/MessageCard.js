97dcdc84c5d371d912c491302492ba0c536e68fa
Maryna Voitenko <maryna.voitenko@gmail.com>
2018-08-20 20:34:30 -0400

    add like to message

--- client/src/components/MessageCard.js
+++ client/src/components/MessageCard.js
@@ -1,26 +1,49 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const MessageCard = ({ message }) => {
-  const { sender, recipient, content } = message;
+class MessageCard extends Component {
+  constructor (props) {
+    super(props);
+    this.state = {
+      counter: 0
+    }
+  }
 
-  const longMessage = content.length > 140;
+  handleClick = (event) => {
+    event.preventDefault();
+    console.log(this.state);
+    this.setState({
+      counter: this.state.counter + 1
+    })
+  }
 
-  return (
-    <div className="message-card">
-        { (sender || recipient) ?
-            <div className="message-card-header">
-              {sender ? <span className="sender">From: {sender.name}</span> : null }
+  render () {
+    const { sender, recipient, content } = this.props.message;
 
-              {recipient ? <span className="recipient">To: {recipient.name}</span> : null }
-            </div>
-            : null
-        }
+   const longMessage = content.length > 140;
 
-        <div className="message-card-main">
-          {longMessage ? `${content.slice(0, 140)}...` : content}
-        </div>
-    </div>
-  );
+    return (
+      <div className="message-card">
+          { (sender || recipient) ?
+              <div className="message-card-header">
+                {sender ? <span className="sender">From: {sender.name}</span> : null }
+  
+                {recipient ? <span className="recipient">To: {recipient.name}</span> : null }
+              </div>
+              : null
+          }
+  
+          <div className="message-card-main">
+            {longMessage ? `${content.slice(0, 140)}...` : content}
+          </div>
+          <div>
+            {this.state.counter}
+            <button onClick={this.handleClick}>Like</button>
+          </div>
+          
+      </div>
+    );
+  }
+  
 };
 
 export default MessageCard;
